fe57418a8245c9183b306475d9f370a8fb086cdd
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2024-10-19 08:38:18 +0000

    added urls.py

--- myCircle/myCircle/urls.py
+++ myCircle/myCircle/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('circle/', include('circle.urls')),
+    path("", RedirectView.as_view(url='circle/', permanent=True)),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

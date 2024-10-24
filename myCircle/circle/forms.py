1402a833bcca42ef3198668be9039a7f98c19f9f
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2024-10-24 10:32:40 +0000

    prettified register page

--- myCircle/circle/forms.py
+++ myCircle/circle/forms.py
@@ -7,12 +7,17 @@ from django.forms import ModelForm
 class UserRegistrationFrom(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput, label='Password')
     password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
-    birthday = forms.DateField(widget=forms.SelectDateWidget, label='Birthday', required=False)
+    
+    # Updated Birthday field to use HTML5 date input for modern appearance
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='Birthday', required=False)
+    
     class Meta:
         model = User
         fields = ['username', 'email']
+    
     def clean_password_confirm(self):
         cd = self.cleaned_data
+        # Password matching validation
         if cd['password'] != cd['password_confirm']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password_confirm']
